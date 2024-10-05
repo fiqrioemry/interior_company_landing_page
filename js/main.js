1249ab4574e54f4e3d2f917516bc51af01825be3
fiqrioemry <foemry@gmail.com>
2024-10-05 23:56:23 +0700

    fixing error

--- js/main.js
+++ js/main.js
@@ -27,3 +27,24 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+// scroll reveal animation
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 3000,
+  delay: 600,
+});
+
+// hero
+sr.reveal(".hero__text", { origin: "top" });
+
+// steps
+sr.reveal(".steps__step__1", { distance: "100px", interval: 100 });
+sr.reveal(".steps__step__2", { distance: "200px", interval: 100 });
+sr.reveal(".steps__step__3", { distance: "300px", interval: 100 });
+
+// about
+sr.reveal(".about__text", { origin: "left" });
+
+sr.reveal(".about__img", { origin: "right", delay: 800 });
